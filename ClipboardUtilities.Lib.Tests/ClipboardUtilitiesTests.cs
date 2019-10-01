64947bb600703a086f7be4f296719e7f674e68fd
Harish Supe <hsupe@stamps.com>
2019-09-30 21:16:59 -0700

    Added few more actions.

--- ClipboardUtilities.Lib.Tests/ClipboardUtilitiesTests.cs
+++ ClipboardUtilities.Lib.Tests/ClipboardUtilitiesTests.cs
@@ -1,4 +1,7 @@
-﻿using Xunit;
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+using Xunit.Sdk;
 
 namespace ClipboardUtilities.Lib.Tests
 {
@@ -16,8 +19,85 @@ namespace ClipboardUtilities.Lib.Tests
 		[Fact]
 		public void Sort()
 		{
-			Assert.Equal("1\r\n2\r\n3", _sut.Sort("3\r\n1\r\n2"));
-			Assert.Equal("abc\r\npqr\r\nxyz", _sut.Sort("xyz\r\npqr\r\nabc"));
+			Test(new List<string>() {"3", "1", "2"}, new List<string>() { "1", "2", "3" }, _sut.Sort);
+			Test(new List<string>() { "xyz", "abc", "pqr" }, new List<string>() { "abc", "pqr", "xyz" }, _sut.Sort);
+		}
+
+		[Fact]
+		public void Reverse()
+		{
+			Test(new List<string>() { "abc", "pqr", "xyz" }, new List<string>() { "xyz", "pqr", "abc" }, _sut.Reverse);
+		}
+
+		[Fact]
+		public void RemoveDuplicates()
+		{
+			Test(new List<string>() { "abc", "abc", "xyz" }, new List<string>() { "abc", "xyz"}, _sut.RemoveDuplicates);
+		}
+
+		[Fact]
+		public void ConvertDecimalTo8BytesLowercaseHex()
+		{
+			Test(new List<string>() { "12341", "4324", "234" }, new List<string>() { "00003035", "000010e4", "000000ea" }, _sut.ConvertDecimalTo8BytesLowercaseHex);
+		}
+
+		[Fact]
+		public void ConvertHexToDecimal()
+		{
+			Test(new List<string>() { "0x00003035", "0X000010e4", "000000ea" }, new List<string>() { "12341", "4324", "234" },  _sut.ConvertHexToDecimal);
+		}
+
+		[Fact]
+		public void ToSqlInListQuoted()
+		{
+			Assert.Equal("( '1', '2', '3' )", 
+				_sut.ToSqlInListQuoted(new List<string>() { "1", "2", "3" }.JoinIntoString()));
+		}
+
+		[Fact]
+		public void ToSqlInList()
+		{
+			Assert.Equal("( 1, 2, 3 )",
+				_sut.ToSqlInList(new List<string>() { "1", "2", "3" }.JoinIntoString()));
+		}
+
+		delegate string TestMethod(string input);
+		private void Test(IEnumerable<string> input, IEnumerable<string> expected, TestMethod method)
+		{
+			Assert.Equal(expected.JoinIntoString(), method(input.JoinIntoString()));
+		}
+		[Fact]
+		public void IpAddressToHexNumber()
+		{
+			Test(new List<string>() { "191.168.12.21", "191.200.12.13", "0.0.0.0" }, new List<string>() { "0XBFA80C15", "0XBFC80C0D", "0X00000000" }, _sut.IpAddressToHexNumber);
+		}
+
+
+		[Fact(Skip = "Missing functionality: Bad elements in input set")]
+		public void IpAddressToHexNumberInvalidInput()
+		{
+			Test(new List<string>() { "256.256.256.256" }, new List<string>() { "Invalid 256.256.256.256" },
+				_sut.IpAddressToHexNumber);
+		}
+
+		[Fact]
+		public void HexToIpAddress()
+		{
+			Test(new List<string>() { "0XBFA80C15", "0XBFC80C0D", "0X00000000" }, new List<string>() { "191.168.12.21", "191.200.12.13", "0.0.0.0" }, _sut.HexToIpAddress);
+		}
+		//[Fact]
+		//public void ApplyPattern()
+		//{
+		//	Test(new List<string>() { "$$VAL$$ is $$VAL$$", "Monday", "Tuesday" }, 
+		//		new List<string>() { "Monday is Monday", "Tuesday is Tuesday"}, _sut.ApplyPattern);
+		//}
+	}
+
+	internal static class ExtensionMethods
+	{
+		public static string JoinIntoString<T>(this IEnumerable<T> lines)
+		{
+			return string.Join(Environment.NewLine, lines);
 		}
 	}
 }
--- ClipboardUtilities.Lib/ClipboardUtilities.cs
+++ ClipboardUtilities.Lib/ClipboardUtilities.cs
@@ -1,6 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
+using System.Net.Sockets;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text.RegularExpressions;
+using DotNetIpAddress = System.Net.IPAddress;
+
 
 namespace ClipboardUtilities.Lib
 {
@@ -8,6 +14,15 @@ namespace ClipboardUtilities.Lib
 	{
 		string Trim(string input);
 		string Sort(string input);
+		string Reverse(string input);
+		string RemoveDuplicates(string input);
+		string ConvertDecimalTo8BytesLowercaseHex(string input);
+		string ConvertHexToDecimal(string input);
+		string ToSqlInList(string input, bool includeValuesInQuotes = false);
+		string ToSqlInListQuoted(string input);
+		string IpAddressToHexNumber(string input);
+		string HexToIpAddress(string input);
+		string ApplyPattern(string input);
 	}
 
 	internal static class ExtensionMethods
@@ -17,9 +32,21 @@ namespace ClipboardUtilities.Lib
 			return input.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
 		}
 
-		public static string JoinIntoString<T>(this IEnumerable<T> lines)
+		public static string ToMultiLineString<T>(this IEnumerable<T> lines)
 		{
-			return string.Join(Environment.NewLine, lines);
+			return JoinIntoString(lines, Environment.NewLine);
+		}
+
+		public static string JoinIntoString<T>(this IEnumerable<T> lines, string separator)
+		{
+			return string.Join(separator, lines);
+		}
+
+		public static string SurroundWith(this string input, string begin, string end)
+		{
+			return JoinIntoString(
+				new List<string>() { begin, input, end}, 
+				" ");
 		}
 	}
 
@@ -29,14 +56,139 @@ namespace ClipboardUtilities.Lib
 		{
 			return input.SplitInputIntoLines()
 				.Select(x => x.Trim())
-				.JoinIntoString();
+				.ToMultiLineString();
 		}
 
 		public string Sort(string input)
 		{
 			return input.SplitInputIntoLines()
 				.OrderBy(x => x)
-				.JoinIntoString();
+				.ToMultiLineString();
 		}
+
+		public string Reverse(string input)
+		{
+			return input.SplitInputIntoLines()
+				.Reverse()
+				.ToMultiLineString();
+		}
+
+		public string RemoveDuplicates(string input)
+		{
+			return input.SplitInputIntoLines()
+				.Distinct()
+				.ToMultiLineString();
+		}
+
+		public string ConvertDecimalTo8BytesLowercaseHex(string input)
+		{
+			return input.SplitInputIntoLines()
+				.Select(x => $"{long.Parse(x):x8}")
+				.ToMultiLineString();
+		}
+
+		public string ConvertHexToDecimal(string input)
+		{
+			return input.SplitInputIntoLines()
+				.Select(x => Convert.ToUInt64(x, 16))
+				.ToMultiLineString();
+		}
+
+		public string ToSqlInList(string input, bool includeValuesInQuotes = false)
+		{
+			return input.SplitInputIntoLines()
+				.Select(x => includeValuesInQuotes ? $"'{x}'," : $"{x},")
+				.JoinIntoString(" ")
+				.TrimEnd(',')
+				.SurroundWith("(", ")");
+		}
+
+		public string ToSqlInListQuoted(string input)
+		{
+			return ToSqlInList(input, true);
+		}
+
+		public string IpAddressToHexNumber(string input)
+		{
+			return input.SplitInputIntoLines()
+				.Select(x => new IpAddress().ToHexNumberAsString(x))
+				.ToMultiLineString();
+		}
+
+		public string HexToIpAddress(string input)
+		{
+			return input.SplitInputIntoLines()
+				.Select(x => new IpAddress().ToString(x))
+				.ToMultiLineString();
+		}
+
+		public string ApplyPattern(string input)
+		{
+			//input = input.Remove(0, input.IndexOf(System.Environment.NewLine));
+			//return Converter(
+			//	input,
+			//	x => pattern.Replace("$$VAL$$", x)
+			//);
+			return input;
+		}
+	}
+
+	public class IpAddress
+	{
+		public string ToHexNumberAsString(string ipAddress)
+		{
+			return ByteArrayToHexString(ToNumber(ipAddress));
+		}
+
+		public string ToString(string ipAddressAsHexNumber)
+		{
+			return ToString(HexStringToByteArray(ipAddressAsHexNumber));
+		}
+
+		public byte[] ToNumber(string ipAddress)
+		{
+			try
+			{
+				return DotNetIpAddress.Parse(ipAddress).GetAddressBytes();
+			}
+			catch (Exception e)
+			{
+				//log.InfoFormat($"{typeof(IpAddress).FullName}: {ipAddress}. {e}");
+				throw;
+			}
+		}
+
+		public string ToString(byte[] ipAddress)
+		{
+			try
+			{
+				DotNetIpAddress dotNetIpAddress = new DotNetIpAddress(ipAddress);
+
+				if (dotNetIpAddress.AddressFamily == AddressFamily.InterNetwork)
+					return dotNetIpAddress.MapToIPv4().ToString();
+				else if (dotNetIpAddress.AddressFamily == AddressFamily.InterNetworkV6)
+					return dotNetIpAddress.MapToIPv6().ToString();
+				else
+					throw new ArgumentException(string.Format("Ip address belongs to {0} which is not supported.", dotNetIpAddress.AddressFamily));
+			}
+			catch (Exception e)
+			{
+				//log.InfoFormat(string"ByteArrayToHexString(ipAddress)}. {e}");
+				throw;
+			}
+		}
+		private static string ByteArrayToHexString(byte[] bytes)
+		{
+			return ("0x" + BitConverter.ToString(bytes).Replace("-", string.Empty)).ToUpper();
+		}
+
+		private byte[] HexStringToByteArray(string hexString)
+		{
+			hexString = Regex.Replace(hexString, "0x", string.Empty, RegexOptions.IgnoreCase);
+			SoapHexBinary shb = SoapHexBinary.Parse(hexString);
+			return shb.Value;
+		}
+
+		//private static log4net.ILog log = log4net.LogManager.GetLogger(typeof(IpAddress));
 	}
 }
