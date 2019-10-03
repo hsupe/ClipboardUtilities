d46b8ca1e4ff938d73c1af9c024fcba069e30a33
Harish Supe <hsupe@stamps.com>
2019-10-03 07:05:25 -0700

    Replaced MutexManager with a simpler but sufficient synchronization to ensure single instance of the application.

--- HandyManny/Program.cs
+++ HandyManny/Program.cs
@@ -1,31 +1,47 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Linq;
 using System.Windows.Forms;
-using MutexManager;
 
 
 // Most of this code is adapted from https://www.simple-talk.com/dotnet/.net-framework/creating-tray-applications-in-.net-a-practical-guide/
 namespace HandyManny
 {
-    // Framework for restricting app to a single instance and for running as a tray app.
-    static class Program
-    {
-        [STAThread]
-        static void Main()
-        {
-            if (!SingleInstance.Start()) { return; }
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            try
-            {
-                var applicationContext = new CustomApplicationContext();
-                Application.Run(applicationContext);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message, "Program Terminated Unexpectedly",
-                    MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-            SingleInstance.Stop();
-        }
-    }
+	// Framework for restricting app to a single instance and for running as a tray app.
+	static class Program
+	{
+		[STAThread]
+		static void Main()
+		{
+			if (InstanceAlreadyRunnig())
+				return;
+
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+
+			try
+			{
+				var applicationContext = new CustomApplicationContext();
+				Application.Run(applicationContext);
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message, "Program Terminated Unexpectedly",
+					MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+		}
+
+		private static bool InstanceAlreadyRunnig()
+		{
+			// This is not perfect Mutex - if two instances launch exactly same time, they may see each other running and both quit - live locking.
+			// But this is simple and serves the purpose quite well.
+
+			string thisProcessName = Process.GetCurrentProcess().ProcessName;
+
+			if (Process.GetProcesses().Count(p => p.ProcessName == thisProcessName) > 1)
+				return true;
+
+			return false;
+		}
+	}
 }
