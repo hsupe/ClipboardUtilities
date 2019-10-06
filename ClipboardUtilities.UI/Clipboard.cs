5ced59e2dca4fea0e0468e650dd8f3a41d55e2e3
Harish Supe <hsupe@stamps.com>
2019-10-05 18:21:42 -0700

    Renamed MyClipboard to Clipboard. Also removed dependency on IClipboardUtilities

---
+++ ClipboardUtilities.UI/Clipboard.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using SystemClipboard = System.Windows.Forms.Clipboard;
+
+namespace ClipboardUtilities.UI
+{
+	public class Clipboard
+	{
+		public static string Text
+		{
+			get => RemoveEmptyLines(SystemClipboard.GetText(TextDataFormat.Text));
+			set => SystemClipboard.SetText(value, TextDataFormat.Text);
+		}
+		public static string RemoveEmptyLines(string input) => Regex.Replace(input, @"^\s+$[\r\n]*", string.Empty, RegexOptions.Multiline);
+	}
+}
